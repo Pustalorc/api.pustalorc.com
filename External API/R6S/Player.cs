3f432e3073876c19c2af405b838b45319021006a
Vicente Pastor <persiafighter@gmail.com>
2019-11-12 06:37:50 +0000

    Fix renaming issue

--- External API/R6S/Player.cs
+++ External API/R6S/Player.cs
@@ -2,13 +2,13 @@
 {
     public class Player
     {
-        public string PId { get; set; }
-        public string PName { get; set; }
-        public int PLevel { get; set; }
-        public string PPlatform { get; set; }
-        public string PUser { get; set; }
-        public int PCurrentmmr { get; set; }
-        public int PCurrentrank { get; set; }
+        public string P_Id { get; set; }
+        public string P_Name { get; set; }
+        public int P_Level { get; set; }
+        public string P_Platform { get; set; }
+        public string P_User { get; set; }
+        public int P_Currentmmr { get; set; }
+        public int P_Currentrank { get; set; }
         public int Verified { get; set; }
         public int Kd { get; set; }
     }
